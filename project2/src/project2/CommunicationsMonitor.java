763d017d4936a0c76462edad2126784efc05a1dc
kaminisaldanha <kaminisaldanha@gmail.com>
2019-04-20 15:30:52 -0500

    repo should be fully working now

---
+++ project2/src/project2/CommunicationsMonitor.java
@@ -0,0 +1,103 @@
+package project2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The CommunicationsMonitor class represents the graph G
+ * built to answer infection queries.
+ *
+ * @author Meghna Vaidya
+ * @author Smruthi Sandhanam
+ * @author Kamini Saldanha
+ */
+public class CommunicationsMonitor {
+
+	private List<ComputerNode> compNodes; 
+	
+    /**
+     * Constructor with no parameters
+     */
+    public CommunicationsMonitor() {
+    	compNodes = new ArrayList<ComputerNode>();
+    }
+
+    /**
+     * Takes as input two integers c1, c2, and a timestamp. This triple represents the fact that the computers with IDs
+     * c1 and c2 have communicated at the given timestamp. This method should run in O(1) time. Any invocation of this
+     * method after createGraph() is called will be ignored.
+     *
+     * @param c1        First ComputerNode in the communication pair.
+     * @param c2        Second ComputerNode in the communication pair.
+     * @param timestamp Time the communication took place.
+     */
+    public void addCommunication(int c1, int c2, int timestamp) {
+    	
+    	//creating the computer node and its neighbor
+    	ComputerNode node = new ComputerNode(c1, timestamp);
+    	ComputerNode neighbor = new ComputerNode(c2, timestamp);
+    	
+    	//add timestamp and neighbor for the node
+    	node.addNeighbor(neighbor);
+    	
+    	//add the node to the list of computer nodes
+    	compNodes.add(node);
+
+    }
+
+    /**
+     * Constructs the data structure as specified in the Section 2. This method should run in O(n + m log m) time.
+     */
+    public void createGraph() {
+    }
+
+    /**
+     * Determines whether computer c2 could be infected by time y if computer c1 was infected at time x. If so, the
+     * method returns an ordered list of ComputerNode objects that represents the transmission sequence. This sequence
+     * is a path in graph G. The first ComputerNode object on the path will correspond to c1. Similarly, the last
+     * ComputerNode object on the path will correspond to c2. If c2 cannot be infected, return null.
+     * <p>
+     * Example 3. In Example 1, an infection path would be (C1, 4), (C2, 4), (C2, 8), (C4, 8), (C3, 8)
+     * <p>
+     * This method can assume that it will be called only after createGraph() and that x <= y. This method must run in
+     * O(m) time. This method can also be called multiple times with different inputs once the graph is constructed
+     * (i.e., once createGraph() has been invoked).
+     *
+     * @param c1 ComputerNode object to represent the Computer that is hypothetically infected at time x.
+     * @param c2 ComputerNode object to represent the Computer to be tested for possible infection if c1 was infected.
+     * @param x  Time c1 was hypothetically infected.
+     * @param y  Time c2 is being tested for being infected.
+     * @return List of the path in the graph (infection path) if one exists, null otherwise.
+     */
+    public List<ComputerNode> queryInfection(int c1, int c2, int x, int y) {
+        return null;
+    }
+
+    /**
+     * Returns a HashMap that represents the mapping between an Integer and a list of ComputerNode objects. The Integer
+     * represents the ID of some computer Ci, while the list consists of pairs (Ci, t1),(Ci, t2),..., (Ci, tk),
+     * represented by ComputerNode objects, that specify that Ci has communicated with other computers at times
+     * t1, t2,...,tk. The list for each computer must be ordered by time; i.e., t1\<t2\<...\<tk.
+     *
+     * @return HashMap representing the mapping of an Integer and ComputerNode objects.
+     */
+    public HashMap<Integer, List<ComputerNode>> getComputerMapping() {
+        return null;
+    }
+
+    /**
+     * Returns the list of ComputerNode objects associated with computer c by performing a lookup in the mapping.
+     *
+     * @param c ID of computer
+     * @return ComputerNode objects associated with c.
+     */
+    public List<ComputerNode> getComputerMapping(int c) {
+        return null;
+    }
+    
+    public List<ComputerNode> sortList(int c) {
+        return null;
+    }
+}
---
+++ project2/src/project2/ComputerNode.java
@@ -0,0 +1,63 @@
+package project2;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The ComputerNode class represents the nodes of the graph G, which are pairs (Ci, t).
+ *
+ * @author Smruthi Sandhanam
+ * @author Kamini Saldanha
+ * @author Meghna Vaidya
+ */
+public class ComputerNode {
+	
+	private int timestamp;
+	private int ID;
+	private List<ComputerNode> neighbors;
+	
+	public ComputerNode(int ID, int timestamp) {
+		neighbors = new ArrayList<ComputerNode>();
+		this.ID = ID;
+		this.timestamp = timestamp;
+	}
+	
+	/**
+     * Returns the ID of the associated computer.
+     *
+     * @return Associated Computer's ID
+     */
+    public int getID() {
+        return this.ID;
+    }
+    
+    public void setID(int ID) {
+    	this.ID = ID;
+    }
+
+    /**
+     * Returns the timestamp associated with this node.
+     *
+     * @return Timestamp for the node
+     */
+    public int getTimestamp() {
+        return this.timestamp;
+    }
+    
+    public void setTimestamp(int timestamp) {
+    	this.timestamp = timestamp;
+    }
+    
+    public void addNeighbor(ComputerNode neighbor) {
+    	this.neighbors.add(neighbor);
+    }
+
+    /**
+     * Returns a list of ComputerNode objects to which there is outgoing edge from this ComputerNode object.
+     *
+     * @return a list of ComputerNode objects that have an edge from this to the nodes in the list.
+     */
+    public List<ComputerNode> getOutNeighbors() {
+        return this.neighbors;
+    }
+
+}
