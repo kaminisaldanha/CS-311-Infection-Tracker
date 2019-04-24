b47f5a4d8d11c42c5f3c5c29f670b83a58cd52c6
meghnavaidya <mvaidya@iastate.edu>
2019-04-23 22:03:48 -0500

    Clean up

--- project2/src/project2/CommunicationsMonitorTest.java
+++ project2/src/project2/CommunicationsMonitorTest.java
@@ -18,15 +18,7 @@ public class CommunicationsMonitorTest {
         // Start each test with an empty CommunicationsMonitor
     }
     
-    CommunicationsMonitor cm;
-	@Before
-	public void before() {
-		cm = new CommunicationsMonitor();
-		cm.addCommunication(1, 2, 4);
-		cm.addCommunication(2, 4, 8);
-		cm.addCommunication(3, 4, 8);
-		cm.addCommunication(1, 4, 12);
-	}
+ 
     
     /*
      * Tests if the adjacency list is created properly using example 1 computer nodes
@@ -148,11 +140,12 @@ public class CommunicationsMonitorTest {
         assertEquals(null, monitor.getComputerMapping(3));
     }
     
-    //no work
     @Test
 	public void testCreateGraph() {
-		cm.createGraph();
-		HashMap<Integer, List<ComputerNode>> adjList = cm.getComputerMapping();
+        monitor = createExample1();
+
+		monitor.createGraph();
+		HashMap<Integer, List<ComputerNode>> adjList = monitor.getComputerMapping();
 		//C1
 		List<ComputerNode> c1 = adjList.get(1);
 		assertEquals(2, c1.size());
@@ -200,9 +193,9 @@ public class CommunicationsMonitorTest {
 			//C4[0]
 		List<ComputerNode> c4Neighbors = c4.get(0).getOutNeighbors();
 		assertEquals(3, c4Neighbors.size());
-		assertEquals(2, c4Neighbors.get(0).getID());
+		assertEquals(3, c4Neighbors.get(0).getID());
 		assertEquals(8, c4Neighbors.get(0).getTimestamp());
-		assertEquals(3, c4Neighbors.get(1).getID());
+		assertEquals(2, c4Neighbors.get(1).getID());
 		assertEquals(8, c4Neighbors.get(1).getTimestamp());
 		assertEquals(4, c4Neighbors.get(2).getID());
 		assertEquals(12, c4Neighbors.get(2).getTimestamp());
@@ -217,9 +210,11 @@ public class CommunicationsMonitorTest {
     //no work
     @Test
 	public void testQueryInfectionExample1() {
-		cm.createGraph();
-		List<ComputerNode> path = cm.queryInfection(1, 3, 2, 9);
-		HashMap<Integer, List<ComputerNode>> adjList = cm.getComputerMapping();
+    	monitor = createExample1();
+
+		monitor.createGraph();
+		List<ComputerNode> path = monitor.queryInfection(1, 3, 2, 9);
+		HashMap<Integer, List<ComputerNode>> adjList = monitor.getComputerMapping();
 		
 		assertEquals(adjList.get(1).get(0), path.get(0));
 		assertEquals(adjList.get(2).get(0), path.get(1));
@@ -231,30 +226,36 @@ public class CommunicationsMonitorTest {
     //no work
     @Test
 	public void testQueryInfectionNoNode() {
-		cm.createGraph();
-		List<ComputerNode> path = cm.queryInfection(1, 3, 2, 9);
+        monitor = createExample1();
+
+		monitor.createGraph();
+		List<ComputerNode> path = monitor.queryInfection(1, 3, 2, 9);
 		
-		path = cm.queryInfection(10, 4, 0, 10);
+		path = monitor.queryInfection(10, 4, 0, 10);
 		assertEquals(null, path);
 	}
 	
 	@Test
 	public void testQueryInfectionNoPath() {
-		cm.createGraph();
-		List<ComputerNode> path = cm.queryInfection(1, 3, 2, 9);
+        monitor = createExample1();
+
+		monitor.createGraph();
+		List<ComputerNode> path = monitor.queryInfection(1, 3, 2, 9);
 		
-		path = cm.queryInfection(1, 4, 2, 7);
+		path = monitor.queryInfection(1, 4, 2, 7);
 		assertEquals(null, path);
 	}
 	
 	//no work
 	@Test
 	public void testQueryInfectionShortPath() {
-		cm.createGraph();
-		List<ComputerNode> path = cm.queryInfection(1, 3, 2, 9);
-		HashMap<Integer, List<ComputerNode>> adjList = cm.getComputerMapping();
+        monitor = createExample1();
+
+		monitor.createGraph();
+		List<ComputerNode> path = monitor.queryInfection(1, 3, 2, 9);
+		HashMap<Integer, List<ComputerNode>> adjList = monitor.getComputerMapping();
 		
-		path = cm.queryInfection(1, 4, 2, 8);
+		path = monitor.queryInfection(1, 4, 2, 8);
 		assertEquals(adjList.get(1).get(0), path.get(0));
 		assertEquals(adjList.get(2).get(0), path.get(1));
 		assertEquals(adjList.get(2).get(1), path.get(2));
